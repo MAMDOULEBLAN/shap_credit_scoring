0782187c332f86d3c380cd2a203edc03509e6d00
MAMDOULEBLAN <dmamdou.alain@gmail.com>
2025-04-21 11:38:22 +0200

    Update streamlit_dashboard.py

--- dashboard/streamlit_dashboard.py
+++ dashboard/streamlit_dashboard.py
@@ -12,7 +12,7 @@ st.title("📊 Dashboard - Décision de crédit")
 try:
     #id_response = requests.get("http://localhost:5000/api/ids")
     id_response = requests.get("https://shap-credit-api-mamdou-0a39fd6254f1.herokuapp.com/api/ids")
-    id_response.raise_for_status()
+    #id_response.raise_for_status()
     ids = id_response.json().get("ids", [])
     client_id = st.selectbox("Sélectionnez un identifiant client :", ids)
 except Exception as e:
