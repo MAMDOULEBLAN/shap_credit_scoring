654d0e86f79a79d1544f0fb7939b2a33c8a9d151
MAMDOULEBLAN <dmamdou.alain@gmail.com>
2025-04-28 17:43:01 +0200

    chemin app

--- api/app.py
+++ api/app.py
@@ -4,9 +4,18 @@ import joblib
 import shap
 import os
 
+
+
 app = Flask(__name__)
 
 
+# Charger le modèle
+model_path = os.path.join(os.getcwd(), "model", "best_model.pickle")
+model = joblib.load(model_path)
+
+# Charger les données
+#data_path = os.path.join(os.getcwd(), "data", "sample_full.csv")
+#data = pd.read_csv(data_path)
 
 # Charger le modèle et les données
 model = joblib.load("./model/best_model.pickle")
